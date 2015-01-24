93869a16a9333689a6c1e92258603404c501e6b9
Timo Moisio <timohmoisio@gmail.com>
2015-01-24 15:39:40 +0200

    Add selected color

--- JourneyOnTheWall/Assets/Scripts/SelectableController.cs
+++ JourneyOnTheWall/Assets/Scripts/SelectableController.cs
@@ -8,20 +8,27 @@ namespace JourneyOnTheWall
 	{
 		[SerializeField]
 		private Button button;
+		[SerializeField]
+		private SpriteRenderer spriteRenderer;
+		[SerializeField]
+		private Color selectedColor;
+
+		private Color defaultColor;
 
 		void Start()
 		{
+			defaultColor = spriteRenderer.color;
 			button.onClick.AddListener(() => { GameManager.Instance.SelectCharacter(this); });
 		}
 
 		public void Select()
 		{
-
+			spriteRenderer.color = selectedColor;
 		}
 
 		public void Deselect()
 		{
-
+			spriteRenderer.color = defaultColor;
 		}
 	}
 }
