71cf298c075b66b80c43513f65a6d3855ce42b7d
Timo Moisio <timohmoisio@gmail.com>
2015-01-25 02:53:48 +0200

    Change light animation

--- JourneyOnTheWall/Assets/Scripts/LightController.cs
+++ JourneyOnTheWall/Assets/Scripts/LightController.cs
@@ -30,7 +30,7 @@ namespace JourneyOnTheWall
 
 			tr.localPosition = newPos;
 
-			targetLight.intensity = defaultIntensity + Mathf.Sin(Time.time * 3f) * 0.1f + Mathf.Sin(Time.time * 2.12f) * 0.2f + Mathf.Sin(Time.time * 1.6662f) * 0.3f;
+			targetLight.intensity = defaultIntensity + Mathf.Sin(Time.time * 3f) * 0.1f + Mathf.Sin(Time.time * 2.12f) * 0.2f + Mathf.Sin(Time.time * 1.6662f) * 0.15f;
 		}
 	}
 }
