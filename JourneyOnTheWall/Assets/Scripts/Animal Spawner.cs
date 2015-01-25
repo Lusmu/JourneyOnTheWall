a6b0113bc3172fccaa9149349ada51870f1801cc
Timo Moisio <timohmoisio@gmail.com>
2015-01-25 02:59:58 +0200

    Add animal spawner class

---
+++ JourneyOnTheWall/Assets/Scripts/Animal Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JourneyOnTheWall
+{
+	public class AnimalSpawner : MonoBehaviour 
+	{
+		[SerializeField]
+		private GameObject animalPrefab1;
+		[SerializeField]
+		private float animalChange1 = 0.7f;
+
+		[SerializeField]
+		private GameObject animalPrefab2;
+		[SerializeField]
+		private float animalChange2 = 0.3f;
+
+		void Start ()
+		{
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
